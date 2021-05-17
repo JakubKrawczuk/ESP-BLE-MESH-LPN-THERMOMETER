4ce9ceb5c6fba55d5876bd455917f31ca94dc30e
Jakub Krawczuk <kubkra@gmail.com>
2021-05-17 14:41:03 +0200

    Enabled Bluetooth modem sleep

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/JK/eclipse-workspace/thermometer_lpn_esp_ble_mesh/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/ESP-IDF/esp-idf-v4.2/.git")
+set(GIT_DIR "C:/Users/JK/eclipse-workspace/thermometer_lpn_esp_ble_mesh/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
